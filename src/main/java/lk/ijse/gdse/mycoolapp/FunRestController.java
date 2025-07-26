e7030f916c8dbb0d9d23977c0b0b6f49e2c53474
sasuniii0 <sasuniwijerathne@gmail.com>
2025-07-26 09:56:11 +0530

    add application properties and FunRestController with endpoints

--- src/main/java/lk/ijse/gdse/mycoolapp/FunRestController.java
+++ src/main/java/lk/ijse/gdse/mycoolapp/FunRestController.java
@@ -1,5 +1,23 @@
 package lk.ijse.gdse.mycoolapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
 
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/fun")
+    public String sayFun(){
+        return "Fun World";
+    }
+
+    @GetMapping("/fun2")
+    public String sayFun2(){
+        return "Fun2 World";
+    }
 }
